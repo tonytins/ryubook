490e1449cdb97c57e40eb07414c2b74d9d016da7
Anthony Foxclaw <noreply@tonytins.xyz>
2020-04-16 20:13:00 -0400

    Powershell script
    - Added unlicense and README
    - Tweaked editorconfig

---
+++ README.md
@@ -0,0 +1,13 @@
+# RyuBook
+
+RyuBook is a ePub generator that uses Pandoc for generation.
+
+## Authors
+
+- **Anthony Foxclaw** - _Initial work_ - [tonytins](https://github.com/tonytins)
+
+See also the list of [contributors](https://github.com/tonytins/RyuBook/contributors) who participated in this project.
+
+## License
+
+I dedicate any and all copyright interest in this software to the public domain - see the [UNLICENSE](UNLICENSE) file for details.
--- RyuBook/publish.ps1
+++ RyuBook/publish.ps1
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
-
-if [ "$(uname)" == "Darwin" ]; then
+if($IsMacOS)
+{
     echo "Publishing macOS release."
     dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+}
+elseif ($IsLinux)
+{
     echo "Publishing Linux release."
     dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+}
 else
+{
     echo "Publishing Windows release."
     dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-fi
+}
