ad83c31c2b6f050dc74293e70d4bfdaae48dcce2
Anthony Foxclaw <noreply@tonytins.xyz>
2020-05-05 12:29:18 -0400

    Passing no arguments should no longer return null

--- src/RyuBook/Options.cs
+++ src/RyuBook/Options.cs
@@ -5,8 +5,8 @@ namespace RyuBook
     [Verb("build", HelpText = "Compiles the book as a ePub.")]
     class BuildOption
     {
-        [Option('t', "title")] public string Title { get; set; }
-        [Option('f', "format")] public string Format { get; set; }
+        [Option('t', "title")] public string Title { get; set; } = string.Empty;
+        [Option('f', "format")] public string Format { get; set; } = string.Empty;
     }
 
     [Verb("clean", HelpText = "Removes all books in the /build directory.")]
