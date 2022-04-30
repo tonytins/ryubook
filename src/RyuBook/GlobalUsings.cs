31b60bbd7d9767a19c4e6005433963c6f82fc342
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-04-30 10:57:49 -0400

    Added license headers

--- src/RyuBook/GlobalUsings.cs
+++ src/RyuBook/GlobalUsings.cs
@@ -1,2 +1,4 @@
+// This project is licensed under the BSD 3-Clause license.
+// See the LICENSE file in the project root for more information.
 global using CommandLine;
 global using System.Diagnostics;
--- src/RyuBook/SysCheck.cs
+++ src/RyuBook/SysCheck.cs
@@ -1,3 +1,5 @@
+// This project is licensed under the BSD 3-Clause license.
+// See the LICENSE file in the project root for more information.
 namespace RyuBook;
 
 public struct SysCheck
